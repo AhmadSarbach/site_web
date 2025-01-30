28700f1a1f9c68bd40856e2b109d1aac28b8da92
Ahmad Sarbach <sarbachahmad@gmail.com>
2025-01-30 12:44:04 -0500

    Semaine 3(modifications finales )

--- app/evenement2/page.jsx
+++ app/evenement2/page.jsx
@@ -6,21 +6,21 @@ export default function Evenement2() {
     return <>
         <div className={styles.background}>
             <section className={styles.tour}>
-            <Tour>
-                <h3>21/11/2025 | Madrid | WiZink Center | 18:45 GMT</h3>
-            </Tour>
-            <Tour>
-                <h3>24/11/2025 | Rome | PalaLottomatica | 19:00 GMT</h3>
-            </Tour>
-            <Tour>
-                <h3>27/11/2025 | Amsterdam | Ziggo Dome | 20:30 GMT</h3>
-            </Tour>
-            <Tour>
-                <h3>30/11/2025 | Bruxelles | Forest National | 18:15 GMT</h3>
-            </Tour>
-            <Tour>
-                <h3>03/12/2025 | Lisbonne | Altice Arena | 19:00 GMT</h3>
-            </Tour>
+                <Tour>
+                    <h3>21/11/2025 | Madrid | WiZink Center | 18:45 GMT</h3>
+                </Tour>
+                <Tour>
+                    <h3>24/11/2025 | Rome | PalaLottomatica | 19:00 GMT</h3>
+                </Tour>
+                <Tour>
+                    <h3>27/11/2025 | Amsterdam | Ziggo Dome | 20:30 GMT</h3>
+                </Tour>
+                <Tour>
+                    <h3>30/11/2025 | Bruxelles | Forest National | 18:15 GMT</h3>
+                </Tour>
+                <Tour>
+                    <h3>03/12/2025 | Lisbonne | Altice Arena | 19:00 GMT</h3>
+                </Tour>
             </section>
 
         </div>
